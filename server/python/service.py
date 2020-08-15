589bca28826c697d89a235623e6c599339489de8
become-nice <995581097@qq.com>
2020-08-15 10:00:51 +0800

    Fix function param

--- server/python/service.py
+++ server/python/service.py
@@ -12,5 +12,8 @@ def get_config(filepath):
 if __name__ == "__main__":
    filepath = sys.argv[1]
    config_param = get_config(filepath)
-   service_client = ServerClient(config_param["brokerServiceUrl"],config_param["topics"].split(','))
+   service_client = ServerClient(url=config_param["brokerServiceUrl"],
+                                 topics=config_param["topics"].split(','),
+                                 token=config_param["token"],
+                                 subscription_name=config_param["subscriptionName"])
    service_client.redeliver_message()
